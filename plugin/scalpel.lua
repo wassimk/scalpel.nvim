51614d8377430fd0fcc766cd23eeeaf88f14b35d
Wassim Metallaoui <wassim@metallaoui.com>
2024-05-27 16:05:05 -0500

    Revert "feat: substitute downward then jump to the top for the rest"
    This reverts commit f7bf242a9507fe948efaf544915462c47aa45892.

--- plugin/scalpel.lua
+++ plugin/scalpel.lua
@@ -4,16 +4,8 @@
 
 local function substitute_current_word()
   local word = vim.fn.expand('<cword>')
-  local pattern = ':,$s/\\v(' .. word .. ")//gc|:undojoin|1,''-&&"
-
-  local cursor_move = vim.api.nvim_replace_termcodes(
-    '<Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left>',
-    true,
-    false,
-    true
-  )
-
-  vim.api.nvim_feedkeys(pattern .. cursor_move, 'n', true)
+  local pattern = ':%s/\\v(' .. word .. ')'
+  vim.api.nvim_feedkeys(pattern, 'n', true)
 end
 
 vim.keymap.set('n', '<leader>e', substitute_current_word, { desc = 'Substite current word in file' })
